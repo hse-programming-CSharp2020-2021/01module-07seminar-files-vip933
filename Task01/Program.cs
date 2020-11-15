234ca56c8679738ad6b70df29f4e173f37eab215
Ivan Petrov <68009390+deltareticuli@users.noreply.github.com>
2020-11-16 00:01:26 +0500

    Initial commit

---
+++ Task01/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+
+/*
+ * По массиву A целых чисел со значениями из диапазона [-10;10] создать массив булевских значений L.
+ * Количество элементов в массивах совпадает.
+ * На местах неотрицательных элементов в массиве A
+ * в массиве L стоят значения true, на месте отрицательных – false.
+ * Если хотя бы один из элементов массива А находится вне заданного диапазона - выводить Incorrect Input
+ * в консоль. Гарантируется, что на вход подается N целых чисел.
+ *
+ * Пример входных данных:
+ * 0 -1
+ *
+ * Пример выходных данных:
+ * true false
+ */
+
+namespace _01_07_Files
+{
+    class Program
+    {
+        private const string inputPath = "input.txt";
+        private const string outputPath = "output.txt";
+        
+        static int[] ReadFile(string path)
+        {
+            // TODO: implement this method
+        }
+
+        static bool CheckArray(int[] array)
+        {
+            // TODO: implement this method
+        }
+        
+        static bool[] IntToBoolArray(int[] array)
+        {
+            // TODO: implement this method
+        }
+        
+        static void WriteFile(string path, bool[] array)
+        {
+            // TODO: implement this method
+        }
+
+        // you do not need to fill your file, you can work with console input
+        static void Main(string[] args)
+        {
+            // do not touch
+            FillFile();
+            
+            int[] A;
+            bool[] L;
+            
+            try
+            {
+                A = ReadFile(inputPath);
+                
+                if (!CheckArray(A))
+                    // TODO: implement this case
+                
+                L = IntToBoolArray(A);
+                WriteFile(outputPath, L);
+            }
+            // TODO: catch with meaningful message
+            
+            // do not touch
+            ConsoleOutput();
+        }
+
+        #region Testing methods for Github Classroom, do not touch!
+
+        static void FillFile()
+        {
+            try
+            {
+                File.WriteAllText(inputPath, Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void ConsoleOutput()
+        {
+            try
+            {
+                Console.WriteLine(File.ReadAllText(outputPath));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Task02/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+
+/*
+ * По массиву A целых чисел со значениями из диапазона (1; 10000] создать массив целых чисел B,
+ * в котором на каждой позиции стоит ближайшая степень двойки меньшая значения из массива A у той же позиции.
+ * Например, A = {30, 100, 300} B = {16, 64, 256}.
+ * Если хотя бы один из элементов массива А находится вне заданного диапазона - выводить Incorrect Input
+ * в консоль. Гарантируется, что на вход подается N целых чисел.
+ *
+ * Пример входных данных:
+ * 3 10 20
+ *
+ * Пример выходных данных:
+ * 2 8 16
+ */
+
+namespace Task02
+{
+    class Program
+    {
+        private const string inputPath = "input.txt";
+        private const string outputPath = "output.txt";
+        
+        static int[] ReadFile(string path)
+        {
+            // TODO: implement this method
+        }
+        
+        static bool CheckArray(int[] array)
+        {
+            // TODO: implement this method
+        }
+        
+        static int[] ConvertArray(int[] array)
+        {
+            // TODO: implement this method
+        }
+
+        static void WriteFile(string path, int[] array)
+        {
+            // TODO: implement this method
+        }
+
+        // you do not need to fill your file manually, you can work with console input
+        static void Main(string[] args)
+        {
+            // do not touch
+            FillFile();
+            
+            int[] A;
+            int[] B;
+            
+            try
+            {
+                A = ReadFile(inputPath);
+                
+                if (!CheckArray(A))
+                    // TODO: implement this case
+                
+                B = ConvertArray(A);
+                WriteFile(outputPath, B);
+            }
+            // TODO: catch with meaningful message
+            
+            // do not touch
+            ConsoleOutput();
+        }
+        
+        #region Testing methods for Github Classroom, do not touch!
+
+        static void FillFile()
+        {
+            try
+            {
+                File.WriteAllText(inputPath, Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void ConsoleOutput()
+        {
+            try
+            {
+                Console.WriteLine(File.ReadAllText(outputPath));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        #endregion
+    }
+}
